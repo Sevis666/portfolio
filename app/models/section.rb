01361bafa56a885eb6ea239ec17bb4a3825f4fe2
Sevis <david.robin97@gmail.com>
2016-09-22 21:15:07 +0200

    Add support for display types

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,19 @@
 class Section < ActiveRecord::Base
   has_many :subsections
   has_many :posts
+
+  def fetch_posts
+    case display
+    when "global:all_posts"
+      Post.order(creation_date: :asc).limit(12)
+    when "global:latest_posts"
+      Post.order(creation_date: :desc).limit(12)
+    when "all_posts"
+      Post.where(section_id: id).order(creation_date: :asc)
+    when "latest_posts"
+      Post.where(section_id: id).order(creation_date: :desc)
+    else
+      Post.where(section_id: id, subsection_id: nil)
+    end
+  end
 end
--- app/models/subsection.rb
+++ app/models/subsection.rb
@@ -1,3 +1,14 @@
 class Subsection < ActiveRecord::Base
   has_many :posts
+
+  def fetch_posts
+    case display
+    when "all_posts"
+      Post.where(section_id: section_id, subsection_id: id).order(creation_date: :asc)
+    when "latest_posts"
+      Post.where(section_id: section_id, subsection_id: id).order(creation_date: :desc)
+    else
+      Post.where(section_id: section_id, subsection_id: id)
+    end
+  end
 end
