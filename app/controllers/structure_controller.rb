963fe7502674d222e347110911f1ef17e999c433
Sevis <david.robin97@gmail.com>
2016-10-25 12:05:10 +0200

    Fix duplicates tag slugs with different names

--- app/controllers/structure_controller.rb
+++ app/controllers/structure_controller.rb
@@ -108,8 +108,8 @@ class StructureController < ApplicationController
         tags = info["tags"]["tag"]
         tags = [tags] unless tags.is_a? Array
         tags.each do |tagname|
-          tag = Tag.find_by(name: tagname) || Tag.new(name: tagname)
-          tag.slug = tag.name.downcase.gsub(' ', '-')
+          slug = tagname.downcase.gsub(' ', '-')
+          tag = Tag.find_by(slug: slug) || Tag.new(slug: slug, name: tagname)
           tag.save
           p.tags << tag unless p.tags.include? tag
         end
